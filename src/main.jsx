481288cafa273f6d1b4bf0b6d2d145a0018d73c6
Genezis <ucmessina@alumnostrazos.net>
2025-09-24 23:52:31 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; // CSS globale dell'app
-import { GlobalStyles } from './styles/js/GlobalStyles.js';
+import { GlobalStyles } from '/src/Styles/GlobalStyles.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
