5c602a6795adc7710be9410d784837bbc08fa303
Genezis <ucmessina@alumnostrazos.net>
2025-09-24 23:50:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// converte import.meta.url in percorso file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // per Render
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
