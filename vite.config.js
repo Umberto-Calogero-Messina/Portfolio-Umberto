7d0d988d5505535dc54b3bc873e10a7727bb0555
Genezis <ucmessina@alumnostrazos.net>
2025-10-14 16:32:07 +0200

    +Update Dist render page

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // per Render
+  base: '/', // per Render
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
